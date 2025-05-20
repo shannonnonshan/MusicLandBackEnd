30e6da61c6ab5f1dd680eabeedc49e60e4596399
shannonnonshan <khanhminhdoan04@gmail.com>
2025-05-20 12:13:09 +0700

    port change

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import songRoutes from './routes/songRoutes.js';
 
 dotenv.config();
 
