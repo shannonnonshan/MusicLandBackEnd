768b3775b637267351a3c78b9d24abe50e3cc8f0
shannonnonshan <khanhminhdoan04@gmail.com>
2025-05-20 10:22:23 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import songRoutes from './routes/songRoutes.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch(err => console.error('DB connection error:', err));
